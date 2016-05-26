2fa6adb1eb6e5a84c6d6441acf8ec80f061b91f9
tkmagesh <tkmagesh77@gmail.com>
2016-05-26 17:09:39 +0530

    Day-01 Complete

---
+++ Day-01/02-Closures/spinner.js
@@ -0,0 +1,38 @@
+function getSpinner(){
+	var count = 0;
+	function increment(){
+		return ++count;
+	}
+	function decrement(){
+		return --count;
+	}
+	return {
+		up : increment,
+		down : decrement
+	}
+}
+
+function getSpinner(){
+	var count = 0;
+	return {
+		up : function(){ return ++count; },
+		down : function(){ return --count; }
+	};
+}
+
+
+
+var spinner = getSpinner();
+
+spinner.up() //=> 1
+spinner.up() //=> 2
+spinner.up() //=> 3
+spinner.up() //=> 4
+
+spinner.down() //=> 3
+spinner.down() //=> 2
+spinner.down() //=> 1
+spinner.down() //=> 0
+spinner.down() //=> -1
+
+
