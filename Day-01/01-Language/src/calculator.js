2c7caa4b2643d456ff999a760b2b4fc4b26f2d06
tkmagesh <tkmagesh77@gmail.com>
2016-05-26 15:23:32 +0530

    Click Tracker - Assignment

--- Day-01/01-Language/src/calculator.js
+++ Day-01/01-Language/src/calculator.js
@@ -1,3 +1,25 @@
 function add(x,y){
-	return x + y;
+	function parseArg(n){
+		if (Array.isArray(n)) return add.apply(this, n)
+		if (typeof n === 'function') return parseArg(n());
+		return isNaN(n) ? 0 : parseInt(n,10);
+	}
+	return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) 
+		+ add([].slice.call(arguments,1));
 }
+
+/*;
+1. As a method of obj
+	this -> obj
+
+2. As a function
+	this -> window
+
+3. Using the call()
+
+4. Using the apply()
+
+
+
+
+*/
