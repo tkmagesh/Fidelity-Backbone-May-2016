522ce59c85a23ac20b4be641ff05f54cc1455856
tkmagesh <tkmagesh77@gmail.com>
2016-05-27 12:50:57 +0530

    JavaScript - Complete

---
+++ Day-02/01-Inheritence/Spinner.js
@@ -0,0 +1,24 @@
+//__proto__
+
+//hiding is costly
+
+
+function Spinner(){
+	//this.__count = 0;
+	var count = 0;
+	/*
+	this.up = function(){ return ++count; };
+	this.down = function(){ return --count; };
+	*/
+}
+Spinner.prototype.up = function(){ return ++this.__count; };
+Spinner.prototype.down = function(){ return --this.__count; };
+
+
+
+var s1 = new Spinner();
+s1.up()
+var s2 = new Spinner();
+var s3 = new Spinner();
+var s4 = new Spinner();
+var s5 = new Spinner();
